2a781673a5575730f5de250d082ba27e27ef4246
Shushant321 <shushantupadhyay770@gmail.com>
2025-09-15 22:05:11 +0530

    neww

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,7 +29,7 @@ export const Contact = () => {
     setButtonText("Sending...");
 
     try {
-      const response = await fetch("http://localhost:5000/Contact", {
+      const response = await fetch("https://portfolio-2me8.onrender.com/Contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formDetails),
